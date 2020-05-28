1e268f1a5a1a88dce45b4620b69ac805b926771a
Patrick Rush <patrick.a.rush@gmail.com>
2020-05-28 18:51:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,3 @@
-
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
@@ -13,7 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma
+def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN"
 end
