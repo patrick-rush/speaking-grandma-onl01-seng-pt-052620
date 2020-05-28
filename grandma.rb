e55e54a25d0aaabd6790749d51ca0fff09c352f8
Patrick Rush <patrick.a.rush@gmail.com>
2020-05-28 19:02:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,8 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  end
   elsif phrase.upcase == phrase
     "NO, NOT SINCE 1938!"
-  end
   else
     "HUH?! SPEAK UP, SONNY!"
   end
